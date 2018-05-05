568bbd298358b6d79d69815ca5e0f8f79de4ea7b
Fabian <fabian@correamarques.com.br>
2018-05-05 15:49:09 -0300

    default constructor for task and required field title

--- App/Models/Task.cs
+++ App/Models/Task.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Models
 {
     public class Task
     {
+        public Task()
+        {
+            CreatedAt = DateTime.Now;
+        }
+
         public int Id { get; set; }
         public string Description { get; set; }
+        [Required]
         public string Title { get; set; }
         public bool Completed { get; set; }
         public bool Deleted { get; set; }
