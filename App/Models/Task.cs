de9a0258ef8965140697a23ced11251ac4b54f0b
Fabian <fabian@correamarques.com.br>
2018-05-05 15:25:25 -0300

    model and controller of tasks

---
+++ App/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace App.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+        public bool Deleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
---
+++ App/Models/TaskDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace App.Models
+{
+    public class TaskDbContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public TaskDbContext() : base("name=TaskDbContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<App.Models.Task> Tasks { get; set; }
+    }
+}
