e3d6455517b8b4c7edbd5cabb1fa958f2e0ebf00
Fabian <fabian@correamarques.com.br>
2018-05-05 16:12:28 -0300

    Development: drop and recreate database, after seed database

--- App/Models/TaskDbContext.cs
+++ App/Models/TaskDbContext.cs
@@ -1,25 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Web;
 
 namespace App.Models
 {
     public class TaskDbContext : DbContext
     {
-        // You can add custom code to this file. Changes will not be overwritten.
-        // 
-        // If you want Entity Framework to drop and regenerate your database
-        // automatically whenever you change your model schema, please use data migrations.
-        // For more information refer to the documentation:
-        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
-    
         public TaskDbContext() : base("name=TaskDbContext")
         {
-            Database.SetInitializer<TaskDbContext>(new DropCreateDatabaseAlways<TaskDbContext>());
+#if DEBUG
+            // drop database and recreate always in debug mode, after seed database
+            Database.SetInitializer<TaskDbContext>(new DropCreateDatabaseIfModelChanges<TaskDbContext>());
+            Database.SetInitializer(new SeedDatabtase());
+#endif
         }
 
-        public System.Data.Entity.DbSet<App.Models.Task> Tasks { get; set; }
+        public class SeedDatabtase : DropCreateDatabaseAlways<TaskDbContext>
+        {
+            protected override void Seed(TaskDbContext context)
+            {
+                IList<Task> tasks = new List<Task>
+                {
+                    new Task { Title = "CRUD Task", Description = "A lot of things to do", CreatedAt = DateTime.Now.AddDays(-10), UpdatedAt = DateTime.Now },
+                    new Task { Title = "Model task", Description = "model with attributes", Completed = true, CompletedAt = DateTime.Now, CreatedAt = DateTime.Now.AddDays(-8), UpdatedAt = DateTime.Now },
+                    new Task { Title = "index return fake list of tasks", Completed = true, CompletedAt = DateTime.Now.AddHours(-4), CreatedAt = DateTime.Now.AddDays(-6), UpdatedAt = DateTime.Now.AddHours(-3) }
+                };
+
+                context.Tasks.AddRange(tasks);
+
+                base.Seed(context);
+            }
+        }
+
+        public DbSet<App.Models.Task> Tasks { get; set; }
     }
 }
